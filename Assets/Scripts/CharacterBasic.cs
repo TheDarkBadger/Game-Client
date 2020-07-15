59f07a7ba7e41b3c8422c4c13f343eedd20ba3d6
TheDarkBadger <TheDarkBadger@users.noreply.github.com>
2020-07-15 22:42:24 +0100

    Initial Commit
    Client is capable of connecting to the server and recieving a list of characters

---
+++ Assets/Scripts/CharacterBasic.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterBasic
+{
+    public string DisplayName { get; private set; }
+    public string Region { get; private set; }
+    public int Level { get; private set; }
+
+    public CharacterBasic(string displayName, string region, int level)
+    {
+        DisplayName = displayName;
+        Region = region;
+        Level = level;
+    }
+}
